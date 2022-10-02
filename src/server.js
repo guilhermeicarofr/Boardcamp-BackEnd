670427776ecb9c6e90eb680da3b6ce4c15729850
guilhermeicarofr <guilhermeicarofr@gmail.com>
2022-10-02 16:44:50 -0300

    Initial server setup and Database connection

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
-//initial
+import express from 'express';
+import cors from 'cors';
+
+import connection from './database/database.js';
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+
+
+server.listen(4000, () => {
+    console.log('Server listening on port 4000...');
+  });
