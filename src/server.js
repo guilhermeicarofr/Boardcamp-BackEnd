2b9935f7dcfe9ef5486886fcac4f034872d28ae4
guilhermeicarofr <guilhermeicarofr@gmail.com>
2022-10-02 17:50:49 -0300

    categories Read and Create routes created. Still to be separated on route-middleware-controller architecture

--- src/server.js
+++ src/server.js
@@ -7,10 +7,57 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-server.get('/test', async (req,res) => {
-    const games = await connection.query('SELECT * FROM games;');
-    res.send(games.rows);
-})
+server.get('/categories', async (req,res) => {
+    const categories = await connection.query('SELECT * FROM categories;');
+    res.send(categories.rows);
+});
+
+server.post('/categories', async (req,res) => {
+
+
+    //middleware
+    const categorie = req.body;
+
+    //change to joi schema
+    if(!categorie.name) {
+        return res.sendStatus(400);
+    }
+
+    try {
+        const checkname = await connection.query('SELECT * FROM categories WHERE name=$1', [categorie.name]);
+        if(checkname.rows.length) {
+            return res.sendStatus(409);
+        }
+        res.locals.name = categorie.name;
+        //next();
+    } catch(error) {
+        console.log(error);
+        res.sendStatus(500);        
+    }
+
+
+    //controller
+    const { name } = res.locals;
+
+    try {
+        await connection.query('INSERT INTO categories (name) VALUES ($1);', [name]);
+        res.sendStatus(201);        
+    } catch(error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
 
 server.listen(4000, () => {
     console.log('Server listening on port 4000...');
