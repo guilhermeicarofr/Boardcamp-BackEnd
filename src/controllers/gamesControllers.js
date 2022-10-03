188eceda8f22082fbbdd37e9816b3c948a208880
guilhermeicarofr <guilhermeicarofr@gmail.com>
2022-10-02 21:09:59 -0300

    Read games search feature from query string name input implemented

--- src/controllers/gamesControllers.js
+++ src/controllers/gamesControllers.js
@@ -1,7 +1,22 @@
 import connection from '../database/database.js';
 
 async function readGames (req,res) {
-    const games = await connection.query('SELECT games.*, categories.name AS "categoryName" FROM games JOIN categories ON games."categoryId"=categories.id;');
+
+    const search = req.query;
+
+    if(search.name) {
+        //check SQL INJECTION
+        const games = await connection.query(`SELECT games.*, categories.name AS "categoryName" 
+                                            FROM games JOIN categories 
+                                            ON games."categoryId"=categories.id
+                                            WHERE games.name
+                                            LIKE '${search.name}%';`);
+        return res.send(games.rows);
+    }
+
+    const games = await connection.query(`SELECT games.*, categories.name AS "categoryName" 
+                                        FROM games JOIN categories 
+                                        ON games."categoryId"=categories.id;`);
     res.send(games.rows);
 }
 
@@ -9,7 +24,8 @@ async function createGame (req,res) {
     const { name, image, stockTotal, categoryId, pricePerDay } = res.locals.game;
 
     try {
-        await connection.query('INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1,$2,$3,$4,$5);', [name, image, stockTotal, categoryId, pricePerDay]);
+        await connection.query(`INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay")
+                                VALUES ($1,$2,$3,$4,$5);`, [name, image, stockTotal, categoryId, pricePerDay]);
         res.sendStatus(201);        
     } catch(error) {
         console.log(error);
